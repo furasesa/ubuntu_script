a3d803629493701e88bdf767a33f3abf6f06ca1e
furasesa <angga.furasesa@gmail.com>
2020-11-06 18:05:11 +0700

    fix some missing dependencies

--- ffmpeg_install.sh
+++ ffmpeg_install.sh
@@ -45,40 +45,46 @@ sudo apt-get update -qq && sudo apt-get -y install \
 	zlib1g-dev \
 	nasm \
 	libx264-dev \
+	libx265-dev \
+	libnuma-dev \
+	libvpx-dev \
+	libfdk-aac-dev \
+	libmp3lame-dev \
+	libopus-dev
 
 echo "checking libaom"
 
-if [ -d aom ]
-then
-	echo "libaom source is exits. updating"
-	git -C aom pull> /dev/null
-else
-	git -C aom pull 2> /dev/null || git clone --depth 1 https://aomedia.googlesource.com/aom
-fi
-
-if [ -d aom_build ]
-then
-	echo "aom_build is detected. yes to rebuild"
-	select choices in build skip;
-	do
-		case $choices in
-			build)
-				mkdir -p aom_build
-        			cd aom_build && \
-        			PATH="$HOME/bin:$PATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_SHARED=off -DENABLE_NASM=on ../aom && \
-        			PATH="$HOME/bin:$PATH" make -j4 && \
-        			make install
-				;;
-			skip)
-				echo "skip rebuild libaom. if something wrong, please command or remove --enable-libaom."
-				break
-				;;
-			*)
-				echo "invalid selection"
-				;;
-		esac
-	done
-fi
+#if [ -d aom ] 
+#then
+#	echo "libaom source is exits. updating"
+#	git -C aom pull> /dev/null
+#else
+#	git -C aom pull 2> /dev/null || git clone --depth 1 https://aomedia.googlesource.com/aom
+#fi
+#
+#if [ -d aom_build ]
+#then
+#	echo "aom_build is detected. yes to rebuild"
+#	select choices in build skip;
+#	do
+#		case $choices in
+#			build)
+#				mkdir -p aom_build
+#        			cd aom_build && \
+#        			PATH="$HOME/bin:$PATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_SHARED=off -DENABLE_NASM=on ../aom && \
+#        			PATH="$HOME/bin:$PATH" make -j4 && \
+#        			make install
+#				;;
+#			skip)
+#				echo "skip rebuild libaom. if something wrong, please command or remove --enable-libaom."
+#				break
+#				;;
+#			*)
+#				echo "invalid selection"
+#				;;
+#		esac
+#	done
+#fi
 
 while true; do
 	read -p "Do you wish to install this program? [y/n]" yn
