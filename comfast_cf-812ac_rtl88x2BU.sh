f9bae6bddcced406aea375e48ae3079f4ac4cf50
furasesa <angga.furasesa@gmail.com>
2021-05-29 15:55:25 +0700

    comfast cf-812ac linux driver rtl88x2BU

---
+++ comfast_cf-812ac_rtl88x2BU.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "source homepage: https://github.com/cilynx/"
+apt update
+apt install bc -y
+git clone https://github.com/cilynx/rtl88x2BU_WiFi_linux_v5.3.1_27678.20180430_COEX20180427-5959.git
+cd rtl88x2BU_WiFi_linux_v5.3.1_27678.20180430_COEX20180427-5959
+VER=$(sed -n 's/\PACKAGE_VERSION="\(.*\)"/\1/p' dkms.conf)
+sudo rsync -rvhP ./ /usr/src/rtl88x2bu-${VER}
+sudo dkms add -m rtl88x2bu -v ${VER}
+sudo dkms build -m rtl88x2bu -v ${VER}
+sudo dkms install -m rtl88x2bu -v ${VER}
+sudo modprobe 88x2bu
+
